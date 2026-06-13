3030c8440cf2bfdab311ed11907d6c206a2fb041
arindamdutta-esme <arindamdutta@esmedigital.com>
2026-06-13 07:45:45 +0530

    updated reverse in doubly circular need more logic

--- Basic Data Structure/CircularLinkedList.py
+++ Basic Data Structure/CircularLinkedList.py
@@ -2,6 +2,7 @@ from LinkedList import Node,LinkedList
 
 class CircularLinkedList(LinkedList):
     HEAD = TAIL = None
+    reversed = False
     def __init__(self):
         super().__init__()
     def __newNode(self,data):
@@ -10,6 +11,18 @@ class CircularLinkedList(LinkedList):
         self.TAIL.next = self.HEAD
         self.HEAD.prev = self.TAIL
         self.TAIL.prev = self.HEAD
+    
+    def __deleteFirstNode(self):
+        if(self.HEAD==self.TAIL):
+            self.HEAD = self.TAIL = None
+        else:
+            temp = self.HEAD
+            self.HEAD = self.HEAD.next
+            self.HEAD.prev = self.TAIL
+            temp.next = None
+            temp.prev = None
+            del temp
+
 
     def insertAtBeg(self, data):
         if(self.HEAD==None):
@@ -43,30 +56,73 @@ class CircularLinkedList(LinkedList):
             flag = 0
             while itr!=self.TAIL:
                 counter += 1
-                if(counter==pos): flag = 1; break
                 itr = itr.next
-            itr = itr.next
-            counter += 1
-            if(counter==pos): flag = 1
+                if(counter==pos): flag = 1; break
+            
             if flag:
-                if(itr==self.TAIL): self.insertAtEnd(data); return
                 newNode = Node(data)
                 newNode.next = itr
                 itr.prev.next = newNode
                 newNode.prev = itr.prev
                 itr.prev = newNode
+            else:
+                itr = itr.next
+                counter += 1
+                if(counter==pos): self.insertAtEnd(data)
+
 
     def deleteAtBeg(self):
-        pass
+        self.__deleteFirstNode()
+
     def deleteAtEnd(self):
-        pass
+        if(self.HEAD==self.TAIL):
+            self.__deleteFirstNode()
+        else:
+            temp = self.TAIL
+            self.TAIL = self.TAIL.prev
+            self.TAIL.next = self.HEAD
+            self.HEAD.prev = self.TAIL
+            temp.next = None
+            temp.prev = None
+            del temp
+
     def deleteAt(self,pos):
-        pass
-    def display(self):
+        counter = 1
+        if(counter==pos):self.__deleteFirstNode(); return
+        flag = 0
         itr = self.HEAD
         while itr!=self.TAIL:
-            print(itr.data)
+            counter+=1
             itr = itr.next
+            if counter==pos:flag = 1;break
+
+        if flag:
+            temp = itr
+            itr = itr.next
+            itr.prev = temp.prev
+            temp.prev.next = itr
+            del temp
+        else:
+            counter+=1
+            itr=itr.next
+            if counter==pos:self.deleteAtEnd()
+
+    def reverse(self):
+        self.reversed = not self.reversed
+    
+    def display(self):
+        print("Doubly Circular LinkedList: ")
+        end = self.TAIL
+        itr = self.HEAD
+        if self.reversed:
+            itr = self.TAIL
+            end = self.HEAD
+        while itr!=end:
+            print(itr.data)
+            if self.reversed:
+                itr = itr.prev
+            else:
+                itr = itr.next
         print(itr.data)
     
 list1 = CircularLinkedList()
@@ -78,5 +134,14 @@ list1.insertAtEnd(5)
 list1.insertAt(1,2)
 list1.insertAt(4,2)
 list1.insertAt(22,7)
-
+# list1.display()
+# list1.deleteAt(3)
+# list1.display()
+# list1.deleteAtBeg()
+# list1.display()
+# list1.deleteAtEnd()
+list1.display()
+list1.reverse()
+list1.display()
+list1.reverse()
 list1.display()

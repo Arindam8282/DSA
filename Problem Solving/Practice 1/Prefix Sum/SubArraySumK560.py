91264fc50c9ac521febe9d2ecf106151f1e27bca
arindam8282 <darindam507@gmail.com>
2026-06-23 20:48:41 +0530

    560 need to practice and reinvent

--- Problem Solving/Practice 1/Prefix Sum/SubArraySumK560.py
+++ Problem Solving/Practice 1/Prefix Sum/SubArraySumK560.py
@@ -1,17 +1,16 @@
 def subarraySum(nums: [int], k: int) -> int:
     current_sum = 0
-    prefixList = [0]
     count = 0
     dict1 = {0:1}
     for i in range(len(nums)):
-        current_sum = prefixList[i] + nums[i]
-        prefixList.append(current_sum)
-        if (current_sum-k) not in dict1:
-            dict1[current_sum] = 1
-        else:
+        current_sum += nums[i]
+        if (current_sum-k) in dict1:
+            count+=dict1[current_sum-k]
+        if current_sum in dict1:
             dict1[current_sum] += 1
-            count+=1
+        else:
+            dict1[current_sum] = 1
     return count
 
 
-print(subarraySum([1,1,1],1))
+print(subarraySum([1,1,1],3))
